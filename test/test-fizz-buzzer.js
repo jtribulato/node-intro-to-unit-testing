5c01621838426e5baac90c4c156a0850871a09e5
John Tribulato <jtribulato@gmail.com>
2018-12-18 02:45:00 -0800

    test

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -2,14 +2,16 @@ const expect = require('chai').expect;
 const adder = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function() {
-    it('should add two numbers', function() {
+    it('should be a multiple of 15', function() {
         const normalCases = [
             {a: 5, expected: 0},
             {a: 15, expected: 0},
             {a: 3, expected: 0},
             {a: 7, expected: 0}
           ];
-
+      fizzBuzzer(a) {
+          return "fizzBuzz";
+      }
 
 
     });
