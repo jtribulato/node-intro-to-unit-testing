615569d6649dc08bcca7b47df35c27fc46bd1e9f
John Tribulato <jtribulato@gmail.com>
2018-12-18 11:46:51 -0800

    another commit

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -9,10 +9,22 @@ describe('fizzBuzzer', function() {
     });
 
     it('should return "buzz" for multiples of 5', function() {
-        [ 5, 10, 15, 20 ].forEach(input => {
-            expect(fizzBuzzer(input)).to.equal('buzz');
+        [ 5, 10, 20, 25 ].forEach(input => {
+            expect(fizzBuzzer(input)).to.equal('buzz');   //fails on 15
         });
     });
+
+    it('should return "fizz" for multiples of 3', function() {
+        [ 3, 6, 9, 12 ].forEach(input => {
+            expect(fizzBuzzer(input)).to.equal('fizz');   
+        });
+    });
+    it('should return same number for each input', function() {
+        [ 2, 4, 7, 16 ].forEach(input => {
+            expect(fizzBuzzer(input)).to.equal(input);   
+        });
+    });
+
 
 });
 
