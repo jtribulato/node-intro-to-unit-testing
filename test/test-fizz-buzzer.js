90db5b287ebda65753100fcfd068b3c005d384e0
John Tribulato <jtribulato@gmail.com>
2018-12-18 03:24:24 -0800

    another damn commit

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -3,16 +3,14 @@ const fizzBuzzer = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function() {
     it('should return "fizz-buzz" for multiples of 15', function() {
-        const normalCases = [ 15, 30, 45 ];
-        normalCases.forEach(input => {
+        [ 15, 30, 45 ].forEach(input => {
             expect(fizzBuzzer(input)).to.equal('fizz-buzz');
         });
     });
 
-    it('should return "fizz-buzz" for multiples of 5', function() {
-        const normalCases = [ 5, 10, 15, 20 ];
-        [ 15, 30, 45 ].forEach(input => {
-            expect(fizzBuzzer(input)).to.equal('fizz-buzz');
+    it('should return "buzz" for multiples of 5', function() {
+        [ 5, 10, 15, 20 ].forEach(input => {
+            expect(fizzBuzzer(input)).to.equal('buzz');
         });
     });
 
