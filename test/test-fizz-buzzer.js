314057535b417bcf54dad89272eb4d386677ae2f
John Tribulato <jtribulato@gmail.com>
2018-12-18 03:06:30 -0800

    more saving

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -1,14 +1,20 @@
 const expect = require('chai').expect;
-const adder = require('../fizzBuzzer');
+const fizzBuzzer = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function() {
-    it('should return fizz-buzz for multiples of 15', function() {
+    it('should return "fizz-buzz" for multiples of 15', function() {
         const normalCases = [ 15, 30, 45 ];
         normalCases.forEach(input => {
             expect(fizzBuzzer(input)).to.equal('fizz-buzz');
         });
-            
- 
     });
+
+    it('should return "fizz-buzz" for multiples of 5', function() {
+        const normalCases = [ 5, 10, 15, 20 ];
+        [ 15, 30, 45 ].forEach(input => {
+            expect(fizzBuzzer(input)).to.equal('fizz-buzz');
+        });
+    });
+
 });
 
